8493346e3f7c097838b8a0e992d348aae886875d
hrisav-rj <hrisavraj.10@gmail.com>
2026-05-25 01:39:53 +0530

    practiceSet added

---
+++ 02_variables_datatypes/practiceSet.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+int main(){
+    // WAP to calculate area of rectange
+    // int len, breadth;
+    // printf("Enter Length: ");
+    // scanf("%d", &len);
+    // printf("Enter Breadth: ");
+    // scanf("%d", &breadth);
+    // printf("Area of Rectangle: %d\n", len * breadth);
+
+
+    // WAP to calculate area of circle and modify to find area of cylinder given radius and height
+    // int radius, height;
+    // printf("Enter Radius:");
+    // scanf("%d", &radius);
+    // printf("Area of Circle: %.2f\n", 3.14 * radius * radius);
+    // printf("Enter Height for Cylinder:");
+    // scanf("%d", &height);
+    // printf("Area of Cylinder: %.2f\n", 2 * 3.14 * radius * height);
+
+
+    // WAP to convert Celsius to Fahrenheit
+    // int celsius;
+    // printf("Enter Celsius:");
+    // scanf("%d", &celsius);
+    // float fah = (celsius * 9.0/5.0) + 32;
+    // printf("Celsius to Fahrenheit for %d: %.2f\n", celsius, fah);
+
+
+    // WAP to calculate Simple Interest
+    // float prin, noy, roi, si;
+    // printf("Enter Principal Amount: ");
+    // scanf("%f", &prin);
+    // printf("Enter Number of Years: ");
+    // scanf("%f", &noy);
+    // printf("Enter Rate of Interest: ");
+    // scanf("%f", &roi);
+    // si = prin*noy*roi/100;
+    // printf("Simple Interest: %.2f\n", si);
+
+
+    // WAP
+
+
+    return 0;
+}
