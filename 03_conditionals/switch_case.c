e081c9a0d567ceb60c47ab61b5e3fde41b1ae37c
hrisav-rj <hrisavraj.10@gmail.com>
2026-05-30 01:02:24 +0530

    in conditionals if switch-case added

---
+++ 03_conditionals/switch_case.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    // switch-case statement
+    // int a;
+    // printf("Enter number from 1 to 3: ");
+    // scanf("%d", &a);
+    // // if not mentioned "break" then all the remaining statements will also execute
+    // switch (a){
+    //     case 1:
+    //         printf("You entered 1\n");
+    //         break;
+    //     case 2:
+    //         printf("You entered 2\n");
+    //         break;
+    //     case 3:
+    //         printf("You entered 3\n");
+    //         break;
+    //     default:
+    //         printf("Nothing Matched\n");
+    // }
+    
+    return 0;
+}
