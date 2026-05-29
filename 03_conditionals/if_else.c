7c5716d0987c257fedd9dd9bf8788dc34c84b181
hrisav-rj <hrisavraj.10@gmail.com>
2026-05-30 00:30:57 +0530

    in conditionals if-else added

---
+++ 03_conditionals/if_else.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int main(){
+    // if -> else
+    // if -> else if
+    // if -> else if -> else
+
+    // int age;
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+    // if (age > 65){
+    //     printf("You are a senior citizen and you can drive\n");
+    // }
+    // else if (age >=30 && age <=40){
+    //     printf("You are elder and you can also drive\n");
+    // }
+    // else{
+    //     printf("You are not eligible to drive\n");
+    // }
+
+
+    // Can also use if-else instead of logical operator
+    // && statement example
+    // if (age >= 18){
+    //     if (age <= 60){
+    //         printf("You are perfect for driving!\n");
+    //     }
+    //     else {
+    //         printf("You suck at driving!\n");
+    //     }
+    // }
+
+    // || statement example - (got it)
+    // int a, b;
+    // printf("Enter 1st number: ");
+    // scanf("%d", &a);
+    // printf("Enter 2nd number: ");
+    // scanf("%d", &b);
+    // if(a>10){
+    //     printf("You have passed the exam.\n");
+    // }
+    // else{
+    //     if(b>10){
+    //         printf("You have just passed the exam.\n");
+    //     }
+    // }
+
+
+    
+    return 0;
+}
