827e9d0230fcf99c9d5fdc2f335d50adadcde083
hrisav-rj <hrisavraj.10@gmail.com>
2026-05-31 18:28:03 +0530

    some modification & conditionals added

---
+++ 03_conditionals/practice.c
@@ -0,0 +1,104 @@
+// #include<stdio.h>
+
+int main(){
+    // int a = 10;
+    // if (a = 11){     //here assigment operator is used, not equals to!
+    //     printf("a = 11\n");
+    // }
+    // else {
+    //     printf("a = 10\n");
+    // }
+    
+
+    // WAP to check whether a student is passed or failed
+    // int marks1, marks2, marks3;
+    // printf("Enter your marks for sub1: ");
+    // scanf("%d", &marks1);
+    // printf("Enter your marks for sub2: ");
+    // scanf("%d", &marks2);
+    // printf("Enter your marks for sub3: ");
+    // scanf("%d", &marks3);
+    // if (marks1<33 || marks2<33 || marks3<33){
+    //     printf("You are failed because you have less marks in individual subject\n");
+    // }
+    // else if ((marks1 + marks2 + marks3)/3 < 40){
+    //     printf("You are failed due to average marks not above 40\n");
+    // }
+    // else {
+    //     printf("You are passed!\n");
+    // }
+
+
+    // WAP to calculate income tax
+    // int income;
+    // float tax;
+    // printf("Enter your income: ");
+    // scanf("%d", &income);
+    // if (income<250000){
+    //     tax = 0;
+    // }
+    // else if (income>=250000 && income<=500000){
+    //     tax = 0.05 * (income - 250000);
+    // }
+    // else if (income>500000 && income<1000000){
+    //     tax = 0.05 * (500000 - 250000) + 0.2 * (income - 500000);
+    // }
+    // else {
+    //     tax = 0.05 * (500000 - 250000) * 0.2 * (1000000 - 500000) + 0.3 * (income - 1000000);
+    // }
+    // printf("Your estimated tax will be: %.2f\n", tax);
+
+
+    // WAP to check whether given year is leap or not
+    // int year;
+    // printf("Enter year: ");
+    // scanf("%d", &year);
+    // if ((year%4==0 && year%100!=0) || year%400==0){
+    //     printf("The year (%d) is a leap year!\n", year);
+    // }
+    // else {
+    //     printf("The year (%d) is not a leap year!\n", year);
+    // }
+
+
+    // WAP to check entered character is lowercase or not
+    // char ch;
+    // printf("Enter a character: ");
+    // scanf("%c", &ch);
+    // printf("The ASCII value of %c is %d!\n", ch, ch);
+    // if (ch >= 97 && ch <=122){
+    //     printf("The entered character is lowercase!\n");
+    // }
+    // else {
+    //     printf("The entered character is not lowercase!\n");
+    // }
+
+
+    // WAP to find greatest out of 4 numbers entered by user
+    // int a, b, c, d;
+    // printf("Enter 1st value: ");
+    // scanf("%d", &a);
+    // printf("Enter 2nd value: ");
+    // scanf("%d", &b);
+    // printf("Enter 3rd value: ");
+    // scanf("%d", &c);
+    // printf("Enter 4th value: ");
+    // scanf("%d", &d);
+    // if (a>b && a>c && a>d){
+    //     printf("1st number is greater than the rest!\n");
+    // }
+    // else if (b>a && b>c && b>d){
+    //     printf("2nd number is greater than the rest!\n");
+    // }
+    // else if(c>a && c>b && c>d){
+    //     printf("3rd number is greater than the rest!\n");
+    // }
+    // else{
+    //     printf("4th number is greater than the rest!\n");
+    // }
+
+
+
+
+    return 0;
+}
