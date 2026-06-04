741c1f599f09f556bea2a2bb71460afbadb5e117
hrisav-rj <hrisavraj.10@gmail.com>
2026-06-04 21:26:48 +0530

    loops added

---
+++ 04_loops/practice_while.c
@@ -0,0 +1,103 @@
+#include <endian.h>
+#include<stdio.h>
+
+int main(){
+    // WAP to print multiplication table
+    // int n;
+    // printf("Enter any number: ");
+    // scanf("%d", &n);
+    // for(int i=1; i<=10; i++){
+    //     printf("%d X %d = %d\n", n, i, n*i);
+    // }
+
+
+    // WAP to print multiplication table in reverse order
+    // int n;
+    // printf("Enter any number: ");
+    // scanf("%d", &n);
+    // for (int i=10; i; i--){
+    //     printf("%d X %d = %d\n", n, i, n*i);
+    // }
+
+
+    // WAP to sum all the natural numbers till 'n'
+    // int n, sum = 0;
+    // printf("Enter n: ");
+    // scanf("%d", &n);
+    // int i = 0;
+    // while (i <= n){
+    //     sum += i;
+    //     i++;
+    // }
+    // printf("The sum of first %d numbers is: %d\n", n, sum);
+
+
+    // WAP to sum first n natural numbers: using while loop
+    // int n, i=1;
+    // printf("Enter the number till you want the natural numbers: ");
+    // scanf("%d", &n);
+    // while (i<=n){
+    //     printf("%d ", i);
+    //     i++;
+    // }
+
+
+    // WAP to sum the occurring of the multiplication table of 8
+    // int n, sum=0;
+    // printf("Enter any number: ");
+    // scanf("%d", &n);
+    // for(int i=1; i<=10;i++){
+    //     sum += n*i;
+    // }
+    // printf("The sum is %d\n", sum);
+
+
+    // WAP to calculate factorial (using for loop)
+    // int n, fac=1;
+    // printf("Enter number: ");
+    // scanf("%d", &n);
+    // for (int i=n; i; i--){
+    //     fac *= i;
+    // }
+    // printf("The factorial of %d is %d\n", n, fac);
+
+
+    // WAP to calculate factorial using while loop
+    // int n, fact = 1;
+    // printf("Enter any number: ");
+    // scanf("%d", &n);
+    // int i = n;
+    // while(i){
+    //     fact *= i;
+    //     i--;
+    // }
+    // printf("The factorial of %d id %d.\n", n, fact);
+
+
+    // WAP to check if a number is prime or not
+    // int n, is_prime = 1;
+    // printf("Enter any number: ");
+    // scanf("%d", &n);
+    // if (n<=1){
+    //     is_prime = 0;
+    // }
+    // else {
+    //     for (int i=2; i <= n/2; i++){
+    //         if(n % i == 0){
+    //             is_prime = 0;
+    //             break;
+    //         }
+    //     }
+    // }
+    // if(is_prime)
+    //     printf("%d is a prime number.\n", n);
+    // else
+    //     printf("%d is not a prime number.\n", n);
+
+
+    
+
+
+    
+    return 0;
+}
